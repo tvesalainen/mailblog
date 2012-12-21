a20a9dda92ff52d444d9dd5f7492bb9eb5955889
tkv <timo.vesalainen@iki.fi>
2012-12-21 15:15:26 +0200

    devel

--- web/js/populate.js
+++ web/js/populate.js
@@ -1,4 +1,4 @@
-                /* 
+/* 
  * Copyright (C) 2012 Timo Vesalainen
  *
  * This program is free software: you can redistribute it and/or modify
@@ -15,29 +15,107 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+var pageStack = new Array();
+
 $(document).ready(function(){
 
-        $("#blog").load("/blog");
+    $("#blog").load("/blog", function(){
+        $(".hidden").hide();
+    });
     
-        $("#calendar").load("/blog?calendar=true");
+    $("#calendar").load("/blog?calendar=true", function(){
+        $(".hidden").hide();
+    });
     
-        $("body").click(function(event){
-            if ($(event.target).is("img")) {
-              window.open($(event.target).attr("src")+"&original=true");
-            }
-            if ($(event.target).is('.calendar-menu')) {
-              var x = $(event.target).get(0);
-              var cls = "."+x.id;
-              $(cls).toggle();
-            }
-            if ($(event.target).is('.blog-entry')) {
-              var x = $(event.target).get(0);
-              $("#blog").load("/blog?blog="+x.id);
-            }
-            if ($(event.target).is('#forward')) {
-              var cursor = $("#nextPage").text();
-              $("#blog").load("/blog?cursor="+cursor);
-            }
+    $("body").on("click", "img", function(event){        
+        window.open($(this).attr("src")+"&original=true");
+    });
+
+    $("body").on("click", ".calendar-menu", function(event){        
+        var x = $(this).get(0);
+        var cls = "."+x.id;
+        $(cls).toggle();
+    });
+
+    $("body").on("click", ".blog-entry", function(event){        
+        var x = $(this).get(0);
+        $("#blog").load("/blog?blog="+x.id);
+    });
+
+    $("body").on("click", ".top", function(event){        
+        $("#blog").load("/blog", function(){
+            afterLoad();
         });
+        pageStack.length = 0;
+    });
+
+    $("body").on("click", ".backward", function(event){        
+        var cursor = $("#nextPage").text();
+        if (cursor) 
+        {
+            pageStack.push(cursor);
+            $("#blog").load("/blog?cursor="+cursor, function(){
+                afterLoad();
+            });
+        }
+    });
+
+    $("body").on("click", ".forward", function(event){        
+        if (pageStack.length > 0)
+        {
+            if (pageStack.length == 1)
+            {
+                $("#blog").load("/blog", function(){
+                    afterLoad();
+                });
+            }
+            else
+            {
+                var cursor = pageStack[pageStack.length-2];
+                $("#blog").load("/blog?cursor="+cursor, function(){
+                    afterLoad();
+                });
+            }
+            pageStack.pop();
+        }
+    });
+
     
 });
+
+function afterLoad()
+{
+    var cursor = $("#nextPage").text();
+    if (cursor)
+    {
+        $(".backward").each(function()
+        {
+            $(this).attr("disabled", false);
+        });
+    }
+    else
+    {
+        $(".backward").each(function()
+        {
+            $(this).attr("disabled", true);
+        });
+    }
+    if (pageStack.length > 0)
+    {
+        $(".forward").each(function()
+        {
+            $(this).attr("disabled", false);
+        });
+    }
+    else
+    {
+        $(".forward").each(function()
+        {
+            $(this).attr("disabled", true);
+        });
+    }
+    $(".hidden").each(function()
+    {
+        $(this).hide();
+    });
+}
