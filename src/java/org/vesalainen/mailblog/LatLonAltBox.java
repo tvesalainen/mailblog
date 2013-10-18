4ecabcba9840c29d2842580ee7a1fa79786e3bbd
tkv <timo.vesalainen@iki.fi>
2013-10-18 15:24:22 +0200

    Partly tested

--- src/java/org/vesalainen/mailblog/LatLonAltBox.java
+++ src/java/org/vesalainen/mailblog/LatLonAltBox.java
@@ -13,17 +13,43 @@ import net.opengis.kml.LatLonAltBoxType;
  */
 public class LatLonAltBox
 {
+    private static final double HalfCircle = 180;
+    private static final double FullCircle = 360;
+    private boolean init;
     private double north = -90.0;
     private double south = 90.0;
     private double west = 180.0;
     private double east = -180.0;
+    /**
+     * 
+     * @param center Center location
+     * @param dia diameter in NM
+     */
+    public LatLonAltBox(GeoPt center, double dia)
+    {
+        this(center.getLatitude(), center.getLongitude(), dia);
+    }
+    /**
+     * 
+     * @param latitude
+     * @param longitude
+     * @param dia diameter in NM
+     */
+    public LatLonAltBox(double latitude, double longitude, double dia)
+    {
+        dia = dia / 120;    // 60 NM / 2
+        north = normalize(latitude+dia);
+        south = normalize(latitude-dia);
+        west = normalize(longitude-dia);
+        east = normalize(longitude+dia);
+        init = true;
+    }
 
     public void add(GeoPt location)
     {
         add(location.getLatitude(), location.getLongitude());
     }
     /**
-     * TODO longitude 180???
      * @param latitude
      * @param longitude 
      */
@@ -33,12 +59,60 @@ public class LatLonAltBox
         assert latitude <= 90;
         assert longitude >= -180;
         assert longitude <= 180;
-        north = Math.max(north, latitude);
-        south = Math.min(south, latitude);
-        east = Math.max(east, longitude);
-        west = Math.min(west, longitude);
-    }
+        if (init)
+        {
+            north = Math.max(north, latitude);
+            south = Math.min(south, latitude);
 
+            east = isWestToEast(east, longitude) ? longitude : east;
+            west = !isWestToEast(west, longitude) ? longitude : west;
+        }
+        else
+        {
+            north = latitude;
+            south = latitude;
+            west = longitude;
+            east = longitude;
+            init = true;
+        }
+    }
+    /**
+     * Return area in square degrees
+     * @return 
+     */
+    public double getArea()
+    {
+        double abs = Math.abs(east-west);
+        if (abs > HalfCircle)
+        {
+            abs = FullCircle - abs;
+        }
+        return abs*(north-south);
+    }
+    private static final double normalize(double val)
+    {
+        return ((val + HalfCircle + FullCircle) % FullCircle) - HalfCircle;
+    }
+    /**
+     * Return true if a2 right of a1
+     * @param west
+     * @param east
+     * @return 
+     */
+    private static final boolean isWestToEast(double west, double east)
+    {
+        west += HalfCircle;
+        east += HalfCircle;
+        double d = east-west;
+        if (Math.abs(d) <= HalfCircle)
+        {
+            return d >= 0;
+        }
+        else
+        {
+            return d < 0;
+        }
+    }
     public void populate(LatLonAltBoxType box)
     {
         box.setNorth(north);
